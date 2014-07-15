dd0e97a584559fa2c4f9bdbbab7465d054d48eed
ct <783239571@qq.com>
2014-07-15 17:00:28 +0800

    first commit

---
+++ src/com/uestc/hams/entity/User.java
@@ -0,0 +1,42 @@
+package com.uestc.hams.entity;
+
+public class User implements java.io.Serializable{
+
+	private Long id;
+	private String loginName;
+	private String name;
+	private String gender;
+	private String password;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
